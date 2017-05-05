c277a89b91d5d08227bfee975dd03a2f41617faa
Zach Ahn <engineering@zachahn.com>
2017-05-05 19:48:13 -0400

    Subclass tests from TestCase

--- test/lmkplz_test.rb
+++ test/lmkplz_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class LmkplzTest < Minitest::Test
+class LmkplzTest < TestCase
   def test_that_it_has_a_version_number
     refute_nil ::Lmkplz::VERSION
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "lmkplz"
 
 require "minitest/autorun"
+
+class TestCase < Minitest::Test
+end

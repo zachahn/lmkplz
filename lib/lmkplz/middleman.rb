dcbddd2ffa9a323ce2e501801fd7f32bf2151048
Zach Ahn <engineering@zachahn.com>
2017-05-06 23:39:44 -0400

    Lint ruby code

--- lib/lmkplz/middleman.rb
+++ lib/lmkplz/middleman.rb
@@ -4,12 +4,13 @@ module Lmkplz
 
     ffi_lib "ext/kkttyl/target/release/libkkttyl.dylib"
 
-    callback :success_callback, [:string, :string], :void
-    callback :failure_callback, [:string], :void
+    callback :success_callback, %i[string string], :void
+    callback :failure_callback, %i[string], :void
     callback :end_callback, [], :void
 
-    attach_function :new_cwatch, [:uint64], :pointer
-    attach_function :add_cwatch, [:pointer, :string], :void
-    attach_function :watch_cwatch, [:pointer, :success_callback, :failure_callback, :end_callback], :pointer
+    attach_function :new_cwatch, %i[uint64], :pointer
+    attach_function :add_cwatch, %i[pointer string], :void
+    attach_function :watch_cwatch, \
+      %i[pointer success_callback failure_callback end_callback], :pointer
   end
 end
